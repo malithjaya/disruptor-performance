d3d95b3114d1f3c3e5dfb0b2f093a1c5d76c2f1b
malithjayasinghe <malith.jayasinghe@gmail.com>
2016-05-05 14:25:14 +0530

    abstract load balancer impl (wip)

--- src/main/java/loadbalancer/AbstractLoadBalancer.java
+++ src/main/java/loadbalancer/AbstractLoadBalancer.java
@@ -1,7 +1,23 @@
 package loadbalancer;
 
+import consumer.Consumer;
+import workload.Task;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * Created by malithjayasinghe on 5/5/16.
  */
 public class AbstractLoadBalancer {
+
+    LinkedBlockingQueue<Task> workloadQueueOne = new LinkedBlockingQueue<Task>();
+    LinkedBlockingQueue<Task> workloadQueueTwo = new LinkedBlockingQueue<Task>();
+    LinkedBlockingQueue<Task> workloadQueueThree = new LinkedBlockingQueue<Task>();
+    LinkedBlockingQueue<Task> workloadQueueFour = new LinkedBlockingQueue<Task>();
+    Consumer consumerThreadOne = new Consumer(workloadQueueOne, "Consumer One");
+    Consumer consumerThreadTwo= new Consumer(workloadQueueTwo, "Consumer Two");
+    Consumer consumerThreadThree = new Consumer(workloadQueueThree, "Consumer Three");
+    Consumer consumerThreadFour = new Consumer(workloadQueueFour, "Consumer Four");
+
+  
 }
